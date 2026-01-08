0ef358abe0971dbb88a90a50db951f1610d82a56
sofiiir <sofiiirodas@gmail.com>
2026-01-07 21:08:15 -0800

    Function update

--- Assignments/assignment1/energy-prod-photovolt.R
+++ Assignments/assignment1/energy-prod-photovolt.R
@@ -8,12 +8,12 @@
 #' Energy Produced from a Photovolatic System
 #'
 #' @param area A number indicating solar panel area (m$^2$)
-#' @param an_sol_rad A number indicating annual average solar radiation (kWh m$^{-2}$)
+#' @param annual_avg_sol_rad A number indicating annual average 
+#'                                  solar radiation (kWh m$^{-2}$)
 #'
 #' @returns energy_prod A number indicating energy produced (kWh)
-#' @export
 #'
-#' @examples
+#' @examples 
 #' photovoltaic_syst(area = 100, 
 #'                   an_sol_rad = 1000)
 
@@ -22,9 +22,9 @@
 photovoltaic_syst <- function(area, an_sol_rad) {
   
   r <- 0.2 # panel yield (dimensionless) representing manufacturer
-  H <- 0.75 # performance ratio (dimensionless) accounting for site-specific losses
+  PR <- 0.75 # performance ratio (dimensionless) accounting for site-specific losses
   
-  energy_prod <- (area * r * H * an_sol_rad)
+  energy_prod <- (area * r * annual_avg_sol_rad * PR)
 
   return(energy_prod)
 }
