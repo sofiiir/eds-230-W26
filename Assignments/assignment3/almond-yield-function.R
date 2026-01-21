e9ef78ebd994c14cb526565de55693d9ff565066
sofiiir <sofiiirodas@gmail.com>
2026-01-20 19:42:00 -0800

    Comments

--- Assignments/assignment3/almond-yield-function.R
+++ Assignments/assignment3/almond-yield-function.R
@@ -1,17 +1,17 @@
 #' Almond Yield
 #'
 #' Calculate almond yields by accounting for their temperature and precipitation specifications.
-#' @param clim_data dataframe with climate data (ideally should include month, year, min_temp, and precip)
-#' @param month month of the temp and precip values to be used in the yield equation (available in the clim_data)
-#' @param year year of the temp and precip values (available in the clim_data)
-#' @param min_temp minimum temperature (C) (available in the clim_data)
-#' @param precip precipitation (mm) (available in the clim_data)
+#' @param clim_data a dataframe with climate data (ideally should include month, year, min_temp, and precip)
+#' @param month an integer between 1- 12 representing the month of the temp and precip values to be used in the yield equation (available in the clim_data)
+#' @param year an integer signifying the year of the temp and precip values (available in the clim_data)
+#' @param min_temp numeric value denoting minimum temperature (C) (available in the clim_data)
+#' @param precip numeric value showing precipitation (mm) (available in the clim_data)
 #' 
 #' @author Sofia Rodas
 #' 
 #' @return dataframe of containing year, minimum temperature, precipitation, and almond yield
 
-crop_yield <- function(clim_data, month, year, min_temp, precip) {
+almond_yield <- function(clim_data, month, year, min_temp, precip) {
     
     # select the min temp values in february
     almond_t2 <- clim_data |> 
@@ -48,11 +48,13 @@ crop_yield <- function(clim_data, month, year, min_temp, precip) {
     # iterate over the min_temp and precip columns to get the yield function
     df$almond_yields <-  map2(df$min_temp, df$precip, yield_fun)
     
+    # the class of the new column is a list so set it as a double 
     df$almond_yields <- as.double(df$almond_yields)
     
+    # extract the max, min, and mean 
     max_almond_yield <- max(df$almond_yields)
     min_almond_yield <- min(df$almond_yields)
     mean_almond_yield <- mean(df$almond_yields)
    
-  return(list("Max almond yield:" = max_almond_yield, "Min almond yield:" = min_almond_yield, "Mean almond yield:" = mean_almond_yield))
+  return(list("Max almond yield (unit ton/acres):" = max_almond_yield, "Min almond yield (unit ton/acres):" = min_almond_yield, "Mean almond yield (unit ton/acres):" = mean_almond_yield))
   }
