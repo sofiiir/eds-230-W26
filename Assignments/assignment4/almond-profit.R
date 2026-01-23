078951be77fde6ae5545deedf17fc47e8154f583
sofiiir <sofiiirodas@gmail.com>
2026-01-23 10:33:27 -0800

    Profit model

---
+++ Assignments/assignment4/almond-profit.R
@@ -0,0 +1,21 @@
+#' Almond Profit Function
+#'
+#' Calculate almond profit for the almond industry based on cost of almond, cost of distribution, and cost of production.
+#' 
+#' @param almond_yield a dataframe with climate yield
+#' @param price_almond a numeric value accounting for price per ton of almond($)
+#' @param year an integer signifying the year of the temperature and precipitation values 
+#' @param cost_prod a numeric value denoting cost of production including labor costs, water ($)
+#' @param cost_dist a numeric value showing the distribution costs of almond ($)
+#' @param npv net-profit value
+#' 
+#' @author Sofia Rodas and Henry Oliver
+#' 
+#' @return dataframe containing year, almond yield, price of almonds, cost of production, cost of distribution, and expected profit
+
+almond_profit <- function(almond_yield, price_almond, year, cost_prod, cost_dist, npv) {
+  
+ 
+  
+  return(df)
+}
