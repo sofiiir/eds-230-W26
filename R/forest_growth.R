a5d88e0a55ba20b7cba6da2e068600a4f5714f86
sofiiir <sofiiirodas@gmail.com>
2026-02-27 12:44:48 -0800

    Finish assignment 6

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -2,9 +2,9 @@
 #' @param time time since start
 #' @param C carbon (C)
 #' @param parms - as list with three values, r, K, g
-#' @param r intrinsic growth rate
+#' @param r intrinsic growth rate (kg/year)
 #' @param K carrying capacity (kgC)
-#' @param g linear growth rate after canopy closure
+#' @param g linear growth rate after canopy closure (kg/year)
 #' @param thresh canopy closure threshold (default 50 (kgC))
 #' @return derivative of population with time
 
