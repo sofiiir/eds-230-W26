8e1d57d794b85183d855ba76b9deff1f81fb04af
sofiiir <sofiiirodas@gmail.com>
2026-02-27 10:01:24 -0800

    Forest growth function

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -1,18 +1,18 @@
 #'  Logistic forest growth derivative
 #' @param time time since start
-#' @param C Canopy closure threshold 
-#' @param parms - as list with two values, r, K, g
+#' @param C carbon (C)
+#' @param parms - as list with three values, r, K, g
 #' @param r intrinsic growth rate
-#' @param K carrying capacity (kg C)
+#' @param K carrying capacity (kgC)
 #' @param g linear growth rate after canopy closure
 #' @param thresh canopy closure threshold (default 50 (kgC))
 #' @return derivative of population with time
 
-forestgrowth <- function(Time, C, parms, thresh = 50) {
+forestgrowth <- function(time, C, parms, thresh = 50) {
   
-  if (thresh < 50){
+  if (C < thresh){
   dC <- parms$r * C
-  } else if (thresh >= 50){
+  } else if (C >= thresh){
   dC <- parms$g * (1 - C / parms$K)
 }
   return(list(dC))
