322a0689c4290b7431d1f5dccfcbe7c4d42ef47a
sofiiir <sofiiirodas@gmail.com>
2026-03-10 13:59:44 -0700

    Lecture and hw

--- R/lotvmodK_hunt.R
+++ R/lotvmodK_hunt.R
@@ -4,18 +4,27 @@
 #' @param t  time (days)
 #' @param pop datatype list initial conditions; list with two values prey=number of prey and pred=number of predictor
 #' @param pars datatype list  coefficient in Lotka-Voltera pars$rprey, pars$alpha, pars$eff, par$pmort
-#' @param pred_thresh numeric value of the predator threshold necessary for hunting to be initiated
+#' @param prey_thresh numeric value of the predator threshold necessary for hunting to be initiated
+#'  \emph{rhunt} is the hunting rate of prey population;
 #'  \emph{rprey} is growth rate of prey population;
 #'  \emph{eff} is the rate of ingestion of prey by predators
 #'  \emph{alpha} is a interaction coefficient (higher values greater interaction
-# ’  \emph{pmort}  mortality rate of predictor population
+#'  \emph{pmort}  mortality rate of predictor population
 #' @examples
-#' lotvod(t = 1, pop = list(1, 2), pop = list(0.5, 0.3, 0.2, 0.2))
+#' lotvod(t = 1, pop = list(1, 2), pars = c(rhunt = 0.1,
+#'                                          rprey = 0.95, 
+#'                                          eff = 0.6, 
+#'                                          alpha = 0.01,
+#'                                          pmort = 0.4))
 #'
-#' pars <- c(rprey = 0.5, alpha = 0.3, eff = 0.2, pmort = 0.2)
+#' pars = c(rhunt = 0.1,
+#'          rprey = 0.95, 
+#'          eff = 0.6, 
+#'          alpha = 0.01,
+#'          pmort = 0.4)
 #' currpop <- c(prey = 1, pred = 1)
 #  days = seq(from=1,to=20)
-#' res <- ode(func = lotvmod, y = currpop, times = days, parms = pars)
+#' res <- ode(func = lotvmodK_hunt, y = currpop, times = days, parms = pars)
 #'
 #' @return  lotvmod returns a list containing the following components
 #' \describe{
@@ -23,18 +32,25 @@
 #' \item{dpred}{rate of change of preditor populutation}
 #' }
 
-lotvmodK_hunt <- function(t, pop, pars, pred_thresh, hunt) {
+lotvmodK_hunt <- function(t, pop, pars, prey_thresh = 150) {
   with(as.list(c(pars, pop)), {
     
-    dprey <- rprey * (1 - prey / K) * prey - alpha * prey * pred
+    # # make sure a negative number of animals isn't being used
+    # pred <- max(pred, 0)
+    # prey <- max(prey, 0)
     
-  if (pred <= 0){
-    dpred <- 0
-  }  else if (pred >= pred_thresh) {
-    dpred <- (eff * alpha * prey * pred) - (pmort * pred) - (hunt * pred)
-  } else {
+    # hunt only if there are more prey than the threshold
+    hunt <- if (prey > prey_thresh) rhunt * prey else 0 
+    
+    # change in prey equation 
+    dprey <- (rprey * (1 - prey / K) * prey - alpha * prey * pred) - hunt
+    
+    # change in predator equation 
     dpred <- (eff * alpha * prey * pred) - (pmort * pred)
-  } 
+    
+    # # change in predator is capped at the number of predator and prey that actually exist
+    # dpred <- if (abs(dpred) > abs(pred)) pred else dpred
+    # dprey <- if (abs(dprey) > abs(prey)) prey else dprey
     
     return(list(c(dprey, dpred)))
   })
